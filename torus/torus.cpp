5266a17c719dfb5e599f08b2ee78f580f9f25827
ysuzuki19 <msethuuh7@i.softbank.jp>
2018-11-23 23:07:36 +0900

    edit torus

--- torus/torus.cpp
+++ torus/torus.cpp
@@ -4,50 +4,32 @@
 
 typedef double TYPE;
 
-const TYPE A = 1.0;
+const TYPE A = 5.0;
 const TYPE R = 2.0;
+const TYPE STEP = 0.02;
 const TYPE THETA_MIN = 0;
 const TYPE THETA_MAX = 2*M_PI;
-const TYPE THETA_STEP = 0.1*M_PI;
+const TYPE THETA_STEP = STEP*M_PI;
 const TYPE PHI_MIN = 0;
 const TYPE PHI_MAX = 2*M_PI;
-const TYPE PHI_STEP = 0.1*M_PI;
+const TYPE PHI_STEP = STEP*M_PI;
 
-// Three Dimensional Coodinates
-typedef struct {
-  TYPE x;
-  TYPE y;
-  TYPE z;
-} tdc;
 
 main(){
-  const TYPE 
-  int data_num = i+1;
-  tdc data[100000];
-  TYPE theta;
-  TYPE phi;
-  int i;
+  std::ofstream fout("torus.txt");
 
-  i = 0;
-  theta = THETA_MIN;
+  TYPE theta = THETA_MIN;
   do{
-    phi = PHI_MIN;
+    TYPE phi = PHI_MIN;
     do{
-      data[i].x = (A+R*cos(theta))*cos(phi);
-      data[i].y = (A+R*cos(theta))*sin(phi);
-      data[i].z = R*sin(theta);
+      fout << (A+R*cos(theta))*cos(phi) << ", "
+           << (A+R*cos(theta))*sin(phi) << ", "
+           << R*sin(theta) << std::endl;
       phi += PHI_STEP;
-      i++;
     }while(phi < PHI_MAX);
     theta += THETA_STEP;
   }while(theta < THETA_MAX);
 
-  std::ofstream fout("torus.txt");
-  for (i=0; i<data_num; i++){
-    fout << std::fixed << data[i].x << ", "
-         << data[i].y << ", "
-         << data[i].z << std::endl;
-  }
   fout.close();
-
+  return 0;
 }
